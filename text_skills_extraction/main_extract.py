01888592d03554ef18ca54c42c4cb3e5682e06a2
popoyote <75493535+popoyote@users.noreply.github.com>
2025-04-10 03:14:08 +0300

    Update main_extract.py

--- text_skills_extraction/main_extract.py
+++ text_skills_extraction/main_extract.py
@@ -1,6 +1,10 @@
 import os
 from text_extract import define_type
 from skills_extract import analyze_skills
+from additional_inf_autoextract import phone_number_extract, email_extract
+from additional_inf_input import collect_applicant_data
+# from test import *
+from university_faculty_extract import find_universities, extract_faculty
 
 
 def process_folder(folder_path):
@@ -14,10 +18,21 @@ def process_folder(folder_path):
         if os.path.isfile(file_path):
             # Обрабатываем файл
             text_from_file = define_type(file_path)
-            found_skills = analyze_skills(text_from_file)
-            print("Найдены навыки:")
-            for skill in found_skills:
-                print(f"- {skill}")
+
+            found_skills = analyze_skills(text_from_file) # Навыки
+            phone_num = phone_number_extract(text_from_file) # Номер телефона
+            email_extracted = email_extract(text_from_file) # Почта
+            birth_date = "0000.00.00" # Дата рождения
+            name = "ФИО" # ФИО
+
+            university = find_universities(text_from_file) # Универ
+            faculties = extract_faculty(text_from_file) # Направления
+
+            applicant_data = collect_applicant_data(
+                fio=name, birthdate=birth_date, phone=phone_num, email=email_extracted)
+            print("Найденные ВУЗы:", *university)
+            print("Найденные направления:", *faculties)
+
 
 folder_path = "rezyume"  # Укажите путь к папке с файлами
 process_folder(folder_path)
